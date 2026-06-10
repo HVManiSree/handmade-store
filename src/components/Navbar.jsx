8fbfd5b5373b6b58b8259ac9fa28cca65caddd96
Mani Sree <manisree037@gmail.com>
2026-06-10 11:51:11 +0530

    Add mobile hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,40 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../context/CartContext";
 import logo from "../assets/logo upt.png";
 import "./Navbar.css";
 
 function Navbar() {
   const { cart } = useContext(CartContext);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className="navbar">
       <Link
         to="/"
-        style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "10px",
-          textDecoration: "none",
-        }}
+        className="logo-section"
       >
         <img
           src={logo}
           alt="Aarkriti Logo"
-          style={{
-            width: "55px",
-            height: "55px",
-            objectFit: "contain",
-          }}
+          className="logo"
         />
 
-        <h2
-          style={{
-            margin: 0,
-            color: "#b76034",
-          }}
-        >
-          Aarkriti
-        </h2>
+        <h2>Aarkriti</h2>
       </Link>
 
-      <div className="nav-links">
+      <button
+        className="menu-btn"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </button>
+
+      <div
+        className={`nav-links ${
+          menuOpen ? "active" : ""
+        }`}
+      >
         <Link to="/">Home</Link>
         <Link to="/shop">Shop</Link>
         <Link to="/custom">Custom Orders</Link>
