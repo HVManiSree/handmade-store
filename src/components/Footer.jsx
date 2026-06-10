da6dfcfca6c763a3188f6cbe5f577555d8222392
Mani Sree <manisree037@gmail.com>
2026-06-10 12:54:02 +0530

    Add Instagram to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,22 @@ function Footer() {
     >
       <p>© 2026 Aarkriti</p>
       <p>Handmade with ❤️</p>
+      <p>
+  📷 Instagram:
+  <a
+    href="https://www.instagram.com/art.by_maniiee"
+    target="_blank"
+    rel="noreferrer"
+    style={{
+      color: "#b76034",
+      marginLeft: "5px",
+      textDecoration: "none",
+      fontWeight: "bold",
+    }}
+  >
+    @art.by_maniiee
+  </a>
+</p>
     </footer>
   );
 }
